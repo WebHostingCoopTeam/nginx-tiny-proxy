f37de7ac5683b1f2c5951cb7d5cb40eadbada063
Josh Cox <josh@webhosting.coop>
2017-04-23 12:23:05 -0500

    lean

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "NGINX_HOST is $NGINX_HOST"
+echo "NGINX_DOMAIN is $NGINX_DOMAIN"
+echo "NGINX_PORT is $NGINX_PORT"
+echo "NGINX_SRC_HOST is $NGINX_SRC_HOST"
+echo "NGINX_SRC_PORT is $NGINX_SRC_PORT"
+echo "NGINX_AUTH is $NGINX_AUTH"
+echo "NGINX_USER is $NGINX_USER"
+echo "NGINX_PASS is $NGINX_PASS"
+
 if [ "${NGINX_AUTH}" = "true" ]
   then
     echo NGINX_AUTH is  ${NGINX_AUTH}
