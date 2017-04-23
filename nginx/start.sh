ec180b142245a78aa1eab35695741cc29f37c7bc
Josh Cox <josh@webhosting.coop>
2017-04-23 11:22:50 -0500

    template

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,11 +2,13 @@
 
 if [ "${NGINX_AUTH}" -eq "true" ]
   then
-    envsubst < /assets/proxy-password.conf > /etc/nginx/conf.d/default.conf
+    echo NGINX_AUTH is  ${NGINX_AUTH}
+    envsubst < /assets/proxy-password.template > /etc/nginx/conf.d/default.conf
     sh -c "echo -n '${NGINX_USER}:' >> /etc/nginx/.htpasswd"
     sh -c "echo ${NGINX_PASS}|openssl passwd -apr1 -stdin >>/etc/nginx/.htpasswd"
 else
-    envsubst < /assets/proxy.conf > /etc/nginx/conf.d/default.conf
+    echo NGINX_AUTH is  ${NGINX_AUTH}
+    envsubst < /assets/proxy.template > /etc/nginx/conf.d/default.conf
 fi
 
 nginx -g 'daemon off;'
