3a3152abaad0b257788bdc2e632691582a2dd3f1
Josh Cox <josh@webhosting.coop>
2017-04-23 12:50:51 -0500

    start

--- nginx/start.sh
+++ nginx/start.sh
@@ -11,13 +11,15 @@ echo "NGINX_PASS is $NGINX_PASS"
 
 if [ "${NGINX_AUTH}" = "true" ]
   then
-    echo NGINX_AUTH is  ${NGINX_AUTH}
-    envsubst < /assets/proxy-password.template > /etc/nginx/conf.d/default.conf
+    envsubst '$$NGINX_HOST $$NGINX_DOMAIN $$NGINX_PORT $$NGINX_SRC_HOST $$NGINX_SRC_PORT $$NGINX_AUTH $$NGINX_USER $$NGINX_PASS' \
+      < proxy-password.template \
+      > /etc/nginx/conf.d/default.conf
     sh -c "echo -n '${NGINX_USER}:' >> /etc/nginx/.htpasswd"
     sh -c "echo ${NGINX_PASS}|openssl passwd -apr1 -stdin >>/etc/nginx/.htpasswd"
 else
-    echo NGINX_AUTH is  ${NGINX_AUTH}
-    envsubst < /assets/proxy.template > /etc/nginx/conf.d/default.conf
+    envsubst '$$NGINX_HOST $$NGINX_DOMAIN $$NGINX_PORT $$NGINX_SRC_HOST $$NGINX_SRC_PORT $$NGINX_AUTH $$NGINX_USER $$NGINX_PASS' \
+      < proxy.template \
+      > /etc/nginx/conf.d/default.conf
 fi
 
 nginx -g 'daemon off;'
