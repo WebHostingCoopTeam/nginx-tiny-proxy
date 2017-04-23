2bc3b61c060c78bda5eee0a57c3932df30f2568b
Josh Cox <josh@webhosting.coop>
2017-04-23 10:44:51 -0500

    nginximport

---
+++ nginx/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "${NGINX_AUTH}" -eq "true" ]
+  then
+    envsubst < /assets/proxy-password.conf > /etc/nginx/conf.d/default.conf
+    sh -c "echo -n '${NGINX_USER}:' >> /etc/nginx/.htpasswd"
+    sh -c "echo ${NGINX_PASS}|openssl passwd -apr1 -stdin >>/etc/nginx/.htpasswd"
+else
+    cp /assets/proxy.conf /etc/nginx/conf.d/default.conf
+fi
+
+nginx -g 'daemon off;'
