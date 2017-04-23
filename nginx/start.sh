706ccf961fa0d5fa45c358b8def3204da5b0ab1c
Josh Cox <josh@webhosting.coop>
2017-04-23 13:13:40 -0500

    debug

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "NGINX_HOST is $NGINX_HOST"
-echo "NGINX_DOMAIN is $NGINX_DOMAIN"
-echo "NGINX_PORT is $NGINX_PORT"
-echo "NGINX_SRC_HOST is $NGINX_SRC_HOST"
-echo "NGINX_SRC_PORT is $NGINX_SRC_PORT"
-echo "NGINX_AUTH is $NGINX_AUTH"
-echo "NGINX_USER is $NGINX_USER"
-echo "NGINX_PASS is $NGINX_PASS"
-
-
 if [ "${NGINX_AUTH}" = "true" ]
   then
     envsubst '$$NGINX_HOST $$NGINX_DOMAIN $$NGINX_PORT $$NGINX_SRC_HOST $$NGINX_SRC_PORT $$NGINX_AUTH $$NGINX_USER $$NGINX_PASS' \
@@ -21,6 +11,23 @@ else
     envsubst '$$NGINX_HOST $$NGINX_DOMAIN $$NGINX_PORT $$NGINX_SRC_HOST $$NGINX_SRC_PORT $$NGINX_AUTH $$NGINX_USER $$NGINX_PASS' \
       < /assets/${NGINX_TEMPLATE}.template \
       > /etc/nginx/conf.d/default.conf
+fi
+
+if [ "${NGINX_DEBUG}" = "true" ]
+  then
+    echo "NGINX_HOST is $NGINX_HOST"
+    echo "NGINX_DOMAIN is $NGINX_DOMAIN"
+    echo "NGINX_PORT is $NGINX_PORT"
+    echo "NGINX_SRC_HOST is $NGINX_SRC_HOST"
+    echo "NGINX_SRC_PORT is $NGINX_SRC_PORT"
+    echo "NGINX_AUTH is $NGINX_AUTH"
+    echo "NGINX_USER is $NGINX_USER"
+    echo "NGINX_PASS is $NGINX_PASS"
+    echo '<<CONFIG>>'
+    cat /etc/nginx/conf.d/default.conf
+    echo '<<END_CONFIG>>'
+else
+    echo "Starting $NGINX_HOST"
 fi
 
 nginx -g 'daemon off;'
